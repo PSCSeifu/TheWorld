03f77f9c2c65fd9806b47ca3b58bfe33845ebad0
SEIFU <seifu@pscpayroll.com>
2016-04-15 08:57:35 +0100

    second commit

---
+++ src/TheWorld/Models/Trip.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public string userName { get; set; }
+        public ICollection<Stop> Stops { get; set; }
+    }
+}
